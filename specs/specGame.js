32e03856236e5258eae1e4ee1c4cfd9827095477
Leshii2902 <aleks-gricen@yandex.ru>
2018-08-12 20:30:40 +0300

    исправление ошибки в тесте

--- specs/specGame.js
+++ specs/specGame.js
@@ -7,28 +7,28 @@ describe('Проверка класса Game', function () {
     let game = new Game(board, preview);
     let shoulder1 = new Shoulder1();
     it('Проверка свойств', function () {
-        expect(game.board).toBe(board);
-        expect(game.preview).toBe(preview);
-        expect(game.base_y).toBe(0);
-        expect(game.base_x).toBe(size_x/2 - ((size_x/2)%1) - 1);
-        expect(game.move_interval).toBe(0);
-        expect(game.MOVE_TIME_CONST).toBe(500);
-        expect(game.move_time).toBe(game.MOVE_TIME_CONST);
-        expect(game.figures_list.length).toBe(7);
-        expect(game.figures_list[0]().positions).toEqual(shoulder1.positions);
-        expect(game.figure).toEqual({});
-        expect(game.figure_css).toBe('');
-        expect(game.next_figure).toEqual({});
-        expect(game.next_figure_css).toBe('');
-        for (let i = 0; i < game.colors.length; i++) {
-            expect(game.colors[i]).toMatch(/\w+/);
-        }
-        expect(game.game_status).toBe('stop');
-        expect(game.type_cell_g).toBe('g_cell_');
-        expect(game.type_cell_pr).toBe('pr_cell_');
-        expect(game.lines).toBe(0);
-        expect(game.level).toBe(1);
-        expect(game.score).toBe(0);
+       expect(game.board).toBe(board);
+       expect(game.preview).toBe(preview);
+       expect(game.base_y).toBe(0);
+       expect(game.base_x).toBe(size_x/2 - ((size_x/2)%1) - 1);
+       expect(game.move_interval).toBe(0);
+       expect(game.MOVE_TIME_CONST).toBe(500);
+       expect(game.move_time).toBe(game.MOVE_TIME_CONST);
+       expect(game.figures_list.length).toBe(7);
+       expect(game.figures_list[0]().positions).toEqual(shoulder1.positions);
+       expect(game.figure).toEqual({});
+       expect(game.figure_css).toBe('');
+       expect(game.next_figure).toEqual({});
+       expect(game.next_figure_css).toBe('');
+       for (let i = 0; i < game.colors.length; i++) {
+           expect(game.colors[i]).toMatch(/\w+/);
+       }
+       expect(game.game_status).toBe('stop');
+       expect(game.type_cell_g).toBe('gameboard');
+       expect(game.type_cell_pr).toBe('prev');
+       expect(game.lines).toBe(0);
+       expect(game.level).toBe(1);
+       expect(game.score).toBe(0);
     });
 
     it('Проверка метода checkBlock', function () {
